dff2f8861f7f512512b07601aa44c2255b6f08d2
Oscar Magana-Rivera <Oscar.magana00@gmail.com>
2021-08-07 17:24:59 -0400

    Step 31, cursor-query
    Implemented the function, getCursorPosition(), which queries the terminal for the cursor's position

--- Spike.c
+++ Spike.c
@@ -71,14 +71,35 @@ char editorReadKey() {
   return c;
 }
 
+// Queries the terminal for the position of the cursor */
+int getCursorPosition(int *rows, int *cols) {
+
+  /* "n" queries the terminal for status information while 
+   * "6" asks for the cursor position */
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  printf("\r\n");
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1) {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+
+  editorReadKey();
+
+  return -1;
+}
+
 /* Sets the parameters to the height and width of the terminal window */
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-    editorReadKey();
-    return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
