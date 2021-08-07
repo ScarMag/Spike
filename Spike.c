cc1e63af22c8fc3db407763115a79d5b6e8292c3
Oscar Magana-Rivera <Oscar.magana00@gmail.com>
2021-08-07 19:01:08 -0400

    Step 38, use-abuf
    Updated Output functions to make use of the abuf type and its functions

--- Spike.c
+++ Spike.c
@@ -151,25 +151,31 @@ void abFree(struct abuf *ab) {
 
 /* =============== Output =============== */
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "-_-", 3);
+    abAppend(ab, "-_-", 3);
 
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
+      abAppend(ab, "\r\n", 2);
     }
   }
 }
 
 /* Sets up the editing environment */
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
+  
+  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[H", 3);
 
-  editorDrawRows();
+  editorDrawRows(&ab);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[H", 3);
+
+  /* Writes the buffer's content out to standard output */
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 /* =============== Input =============== */
