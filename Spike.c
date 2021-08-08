3abed5717c00480a7f39d01d97ea8cafc0f2f2d9
Oscar Magana-Rivera <Oscar.magana00@gmail.com>
2021-08-08 16:28:06 -0400

    Step 47, arrow-keys-enum
    Updated all instances of w, a, s, d keys with the corresponding arrow key

--- Spike.c
+++ Spike.c
@@ -15,6 +15,13 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 'a',
+  ARROW_RIGHT = 'd',
+  ARROW_UP = 'w',
+  ARROW_DOWN = 's'
+};
+
 /* =============== Data =============== */
 
 /* Stores the state of the editor */
@@ -86,10 +93,10 @@ char editorReadKey() {
      * character is returned */
     if (seq[0] == '[') {
       switch (seq[1]) {
-        case 'A': return 'w';
-        case 'B': return 's';
-        case 'C': return 'd';
-        case 'D': return 'a';
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
       }
     }
 
@@ -237,17 +244,17 @@ void editorRefreshScreen() {
 
 void editorMoveCursor(char key) {
   switch (key) {
-    case 'a':
+    case ARROW_LEFT:
       E.cx--;
       break;
-    case 'd':
+    case ARROW_RIGHT:
       E.cx++;
       break;
-    case 's':
-      E.cy++;
-      break;
-    case 'w':
+    case ARROW_UP:
       E.cy--;
+      break;
+    case ARROW_DOWN:
+      E.cy++;
       break;
   }
 }
@@ -262,10 +269,10 @@ void editorProcessKeypress() {
       exit(0);
       break;
 
-    case 'w':
-    case 'a':
-    case 's':
-    case 'd':
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
