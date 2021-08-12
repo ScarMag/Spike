496f418c953f6c07cd44626a467fdc5bd3645e19
Oscar Magana-Rivera <Oscar.magana00@gmail.com>
2021-08-12 19:15:10 -0400

    Step 113, incremented-dirty
    Incremented E.dirty in each row operation that makes a change to the text (editorAppendRow(...) and editorRowInsertChar(...))

--- Spike.c
+++ Spike.c
@@ -282,6 +282,7 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
   
   E.numrows++;
+  E.dirty++;
 }
 
 /* Inserts a character into an erow at a given position */
@@ -296,6 +297,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /* =============== Editor Operations =============== */
