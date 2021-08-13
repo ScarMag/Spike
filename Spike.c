089b275634918356d9bd92ba86ca18d8a57d445b
Oscar Magana-Rivera <Oscar.magana00@gmail.com>
2021-08-12 20:08:40 -0400

    Step 114, reset-dirty
    Reset E.dirty to 0 in editorOpen() and in editorSave() to make the (modified) message in the status bar appear and disappear when necessary

--- Spike.c
+++ Spike.c
@@ -378,6 +378,7 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 /* Writes the string returned by editorRowsToString() to disk */
@@ -402,6 +403,7 @@ void editorSave() {
       if (write(fd, buf, len) == len) {
 	close(fd);
 	free(buf);
+	E.dirty = 0;
 	editorSetStatusMessage("%d bytes written to disk", len);
 	return;
       }
